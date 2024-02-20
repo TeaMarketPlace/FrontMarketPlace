36f962ef6823a8e7aee2a9c18938aede77da59a1
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2024-02-20 15:09:32 +0200

    Environment setup with VITE, React, Redux, TypeScript, redux-persist, react-router-dom, Tailwind CSS, and SASS has been successfully completed and configured

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "../App.scss";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-xl font-bold text-blue-500">Welcome Market Place</h1>
+    </>
+  );
+}
+
+export default App;
