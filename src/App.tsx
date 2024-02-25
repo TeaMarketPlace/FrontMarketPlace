7a47ece1a0421f946040254f8c6a36e47f3e0daa
Roman Humenyak <romangumenyak2003@gmail.com>
2024-02-25 19:38:59 +0200

    install dependencies for form and validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+
+
 import "./App.scss";
 
 function App() {
   return (
-    <>
+    <div className="bg-background">
       <h1 className="text-xl font-bold text-blue-500">Welcome Market Place</h1>
-    </>
+    </div>
   );
 }
 
